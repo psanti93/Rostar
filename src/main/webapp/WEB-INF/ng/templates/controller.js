2bfdd8810f7d666addde99667f246d68f6ec4879
Edward Bafford <edwardbafford@go.rmc.edu>
2016-09-16 09:47:33 -0400

    ANGULARRRRR

--- src/main/webapp/WEB-INF/ng/templates/controller.js
+++ src/main/webapp/WEB-INF/ng/templates/controller.js
@@ -3,15 +3,57 @@
  */
 
 //controllers for each state, can contain variables and functions to help generate template
-angular.module("Rostar").controller("homeCtrl", function($state, $http){
-	
-});
+angular.module("Rostar").controller("homeCtrl", function($state, $http){});
 
 angular.module("Rostar").controller("playerCtrl", function($state, $http){
 	
 });
 
 angular.module("Rostar").controller("coachCtrl", function($state, $http){
+	var cc = this;
+	cc.addPlayer = 0;
+	cc.playerFirstName;
+	cc.playerLastName;
+	cc.coach;
+	cc.position;
+	cc.selection = "Select Position";
+	cc.coaches = [];
+	cc.positions = [];
+	
+	$http({
+		method: "GET",
+		url: "coaches"
+	})
+	.then(
+		function(response){
+			cc.coaches = response.data;
+		}	
+	)
+	
+	$http({
+		method: "GET",
+		url: "positions"
+	})
+	.then(
+		function(response){
+			cc.positions = response.data;
+		}
+	)
+	
+	cc.submit = function(){
+		$http({
+			method: "POST",
+			url: "create/player",
+			data: {
+				  fname: cc.playerFirstName,
+				  lname: cc.playerLastName,
+				  coaches: cc.coach,
+				  position: cc.position
+			}
+		});
+		
+		cc.addPlayer = 0;
+	}
 	
 });
 
